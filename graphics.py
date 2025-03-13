bbab7c5b10ddc3576d3a1a5cd53317f5c29c1fde
Craig Rossiter <crossiter0@gmail.com>
2025-03-13 16:17:29 -0500

    finished layout of window

---
+++ graphics.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("This is the title for the Maze Solver")
+        self.__canvas = tk.Canvas(
+            self.__root,
+            width=width,
+            height=height,
+            bg="white"
+        )
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("Window closed.....")
+    
+    def close(self):
+        self.__running = False
+
+   
+        
+        
+
+
