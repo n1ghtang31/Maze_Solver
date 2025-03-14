509eb04039ec7cc4d691b0d4d9f9e9d13a0924a9
Craig Rossiter <crossiter0@gmail.com>
2025-03-13 21:11:40 -0500

    Added the ability to draw cells

--- graphics.py
+++ graphics.py
@@ -33,6 +33,7 @@ class Window:
         line.draw(self.__canvas, fill_color)
 
 
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -43,13 +44,36 @@ class Line:
         self.p1 = p1
         self.p2 = p2
 
-    def draw(self, canvas, fill_color):
+    def draw(self, canvas, fill_color="black"):
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
 
-   
-        
-        
+class Cell:
+    def __init__(self, _win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = _win
 
-
+    def draw(self, x1, y1, x2, y2):
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+        
+            
+            
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(1600, 900)
-    line1 = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(line1, "Green")
+    line1 = Line(Point(100, 50), Point(400, 400))
+   #win.draw_line(line1, "Green")
+    cell1 = Cell(win)
+    cell2 = Cell(win)
+    cell3 = Cell(win)
+    cell4 = Cell(win)
+    cell1.has_bottom_wall = False
+    cell4.has_top_wall = False
+    cell1.draw(0, 0, 100, 100)
+    cell2.draw(100, 0, 200, 100)
+    cell3.draw(200, 0, 300, 100)
+    cell4.draw(0, 100, 100, 200)
     win.wait_for_close()
 
 
